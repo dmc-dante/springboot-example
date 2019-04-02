6c75c44bc3ec4347530b4ba1a8ade2e0e83abec2
duke <710495412@qq.com.com>
2019-04-02 22:43:40 +0800

    git jsp试验，必须要用 mvn clean spring-boot:run启动！！！

---
+++ demo-jsp/src/main/java/com/springbootexample/demojsp/DemoJspController.java
@@ -0,0 +1,31 @@
+package com.springbootexample.demojsp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+import java.util.Map;
+
+@Controller
+public class DemoJspController {
+	@RequestMapping(value = {"/","/index"})
+	public String index(Map<String, Object> model){
+		// 直接返回字符串，框架默认会去 spring.view.prefix 目录下的 （index拼接spring.view.suffix）页面
+		// 本例为 /WEB-INF/jsp/index.jsp
+		model.put("time", new Date());
+		return "index";
+	}
+
+	/**
+	 * 响应到JSP页面page1
+	 */
+	@RequestMapping("/page1")
+	public ModelAndView page1(){
+		// 页面位置 /WEB-INF/jsp/page/page.jsp
+		ModelAndView mav = new ModelAndView("page/page1");
+		mav.addObject("content", "hello");
+		return mav;
+	}
+
+}
---
+++ demo-jsp/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+    <title>Spring Boot Sample jsp</title>
+</head>
+
+<body>
+Time: ${time}
+<br>
+</body>
+</html>
